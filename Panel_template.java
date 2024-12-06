e75cc84b710fa57e687150b726e6b8ee2c8ad176
Fialuxe <132959114+Fialuxe@users.noreply.github.com>
2024-12-06 23:06:41 +0900

    [Add]コンパイルエラー回避のためにこれから増えるであろうPanelのクラスを作成

---
+++ Panel_template.java
@@ -0,0 +1,10 @@
+import javax.swing.JPanel;
+
+class TitlePanel extends JPanel{}
+class LobbyPanel extends JPanel{}
+class EndPanel extends JPanel{}
+class FloorOnePanel extends JPanel{}
+class FloorTwoPanel extends JPanel{}
+class FloorThreePanel extends JPanel{}
+class FloorFourPanel extends JPanel{}
+class GameOverPanel extends JPanel{}
