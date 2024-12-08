e438f4a26d5abc0540ae4d6e847ca928f0cbb62c
Fialuxe <132959114+Fialuxe@users.noreply.github.com>
2024-12-08 12:56:15 +0900

    [Add]状態の実装と横長コードの改良

--- State.java
+++ State.java
@@ -6,17 +6,23 @@ enum Event{
     STATE_2F,
     STATE_3F,
     STATE_4F,
-    STATE_GAMEOVER
+    STATE_GAMEOVER,
+    STATE_1F_ROOM,
+    STATE_2F_ROOM,
+    STATE_3F_ROOM,
+    STATE_4F_ROOM
 }
 /*
  * @brief
- * 状態を管理するクラス
+ * 状態を管理するクラス。各状態は遷移のためのメソッドtransitionTo、
+ * String型のタイトル獲得用のメソッドgetTitle、状態取得用のgetStateを持つ。
  */
 class State{
     protected String title;
+    //コンストラクタ
     public State(String title) { this.title = title;}
-    public State(){ this.title = "タイトルが指定されていません"; }
-
+    public State(){ this.title = "Untitled"; }
+    //状態を遷移させる
     public State transitionTo(Event event){
         return StateFactory.createState(event);
     }
@@ -24,7 +30,7 @@ class State{
     public String getTitle(){ return title;}
     public Event getState(){
         return StateFactory.getState(this);
-    };
+    }
 }
 
 //タイトル画面の状態を管理するクラス。
@@ -36,7 +42,9 @@ class TitleState extends State{
 
     @Override
     public State transitionTo(Event event){
-        if(event == Event.STATE_LOBBY || event == Event.STATE_END || event ==Event.STATE_GAMEOVER){
+        if( event == Event.STATE_LOBBY || 
+            event == Event.STATE_END || 
+            event ==Event.STATE_GAMEOVER){
             return super.transitionTo(event);
         }else{
             return this;
@@ -54,7 +62,8 @@ class EndState extends State{
     }
     @Override
     public State transitionTo(Event event){
-        if(event == Event.STATE_TITLE || event == Event.STATE_GAMEOVER){
+        if( event == Event.STATE_TITLE || 
+            event == Event.STATE_GAMEOVER){
             return super.transitionTo(event);
         }else{
             return this;
@@ -71,7 +80,11 @@ class LobbyState extends State{
 
     @Override
     public State transitionTo(Event event){
-        if(event == Event.STATE_1F || event == Event.STATE_2F || event == Event.STATE_3F || event == Event.STATE_4F || event == Event.STATE_GAMEOVER){
+        if( event == Event.STATE_1F || 
+            event == Event.STATE_2F || 
+            event == Event.STATE_3F || 
+            event == Event.STATE_4F || 
+            event == Event.STATE_GAMEOVER){
             return super.transitionTo(event);
         }else{
             return this;
@@ -93,13 +106,71 @@ class GameOverState extends State{
 }
 
 class FloorOneState extends State{
+    public FloorOneState(){
+        super("1F");
+    }
+    @Override
+    public State transitionTo(Event event){
+        if( event == Event.STATE_LOBBY ||
+            event == Event.STATE_GAMEOVER || 
+            event == Event.STATE_1F_ROOM){
+            return super.transitionTo(event);
+        }else{
+            return this;
+        }
+    }
 }
 class FloorTwoState extends State{
+    public FloorTwoState(){
+        super("2F");
+    }
+    @Override
+    public State transitionTo(Event event){
+        if( event == Event.STATE_LOBBY || 
+            event == Event.STATE_GAMEOVER || 
+            event == Event.STATE_2F_ROOM){
+            return super.transitionTo(event);
+        }else{
+            return this;
+        }
+    }
 }
 
 class FloorThreeState extends State{
+    public FloorThreeState(){
+        super("3F");
+    }
+    @Override
+    public State transitionTo(Event event){
+        if( event == Event.STATE_LOBBY || 
+            event == Event.STATE_GAMEOVER || 
+            event == Event.STATE_3F_ROOM){
+            return super.transitionTo(event);
+        }else{
+            return this;
+        }
+    }
 }
 
 class FloorFourState extends State{
+    public FloorFourState(){
+        super("4F");
+    }
+    @Override
+    public State transitionTo(Event event){
+        if( event == Event.STATE_LOBBY || 
+            event == Event.STATE_GAMEOVER || 
+            event == Event.STATE_4F_ROOM){
+            return super.transitionTo(event);
+        }else{
+            return this;
+        }
+    }
 }
+
+class OneFloorRoomState extends State{}
+class TwoFloorRoomState extends State{}
+class ThreeFloorRoomState extends State{}
+class FourFloorRoomState extends State{}
+
 
