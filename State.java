e2fc4c65e4a07072f8048779451c95afebb257a8
Fialuxe <132959114+Fialuxe@users.noreply.github.com>
2024-12-08 12:23:33 +0900

    [Edit] 抽象クラスが不要になったのでクラスに変更 & リファクタリング

--- State.java
+++ State.java
@@ -7,31 +7,24 @@ enum Event{
     STATE_3F,
     STATE_4F,
     STATE_GAMEOVER
-    //TODO あとはここに追加していく。ここも切り出したいのが本音。
 }
-
-interface StateInterface{
-    public State transitionTo(Event event);
-    public String getTitle();
-    public abstract Event getState();
-}
-
 /*
  * @brief
- * 状態を管理する抽象クラス
+ * 状態を管理するクラス
  */
-abstract class State implements StateInterface{
+class State{
     protected String title;
-    //コンストラクタ
     public State(String title) { this.title = title;}
     public State(){ this.title = "タイトルが指定されていません"; }
-    //共通メソッド
-    // TitleState sとすると、 s.transitionTo(STATE_LOBBY)のようにすることで、sの状態をLobbyStateに移行できる。
+
     public State transitionTo(Event event){
         return StateFactory.createState(event);
     }
+
     public String getTitle(){ return title;}
-    public abstract Event getState();
+    public Event getState(){
+        return StateFactory.getState(this);
+    };
 }
 
 //タイトル画面の状態を管理するクラス。
@@ -41,10 +34,6 @@ class TitleState extends State{
         super("タイトル画面");
     }
 
-    @Override
-    public Event getState(){
-        return Event.STATE_TITLE;
-    }
     @Override
     public State transitionTo(Event event){
         if(event == Event.STATE_LOBBY || event == Event.STATE_END || event ==Event.STATE_GAMEOVER){
@@ -71,10 +60,6 @@ class EndState extends State{
             return this;
         }
     }
-    @Override
-    public Event getState(){
-        return Event.STATE_END;
-    }
 }
 
 //ロビーの画面を意味する状態。
@@ -83,6 +68,7 @@ class LobbyState extends State{
     public LobbyState(){
         super("ロビー");
     }
+
     @Override
     public State transitionTo(Event event){
         if(event == Event.STATE_1F || event == Event.STATE_2F || event == Event.STATE_3F || event == Event.STATE_4F || event == Event.STATE_GAMEOVER){
@@ -91,59 +77,29 @@ class LobbyState extends State{
             return this;
         }
     }
-
-    @Override
-    public Event getState(){
-        return Event.STATE_LOBBY;
-    }
 }
 
-
-///////////////////////////////
-/// 未実装ぞーん
-///////////////////////////////
+/**
+ * ゲームオーバーの状態を表す状態
+ */
+class GameOverState extends State{
+    public GameOverState(){
+        super("ゲームオーバー");
+    }
+    @Override
+    public State transitionTo(Event event){
+        return this;
+    }
+}
 
 class FloorOneState extends State{
-
-    @Override
-    public Event getState() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getView'");
-    }
-
 }
 class FloorTwoState extends State{
-
-    @Override
-    public Event getState() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getView'");
-    }
 }
 
 class FloorThreeState extends State{
-
-    @Override
-    public Event getState() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getView'");
-    }    
 }
 
 class FloorFourState extends State{
-    @Override
-    public Event getState() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getView'");
-    }
-}
-
-class GameOverState extends State{
-
-    @Override
-    public Event getState() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getView'");
-    }    
 }
 
