d52d9e0d023e6eec746cb8d21a10e2a1a7b483ce
Fialuxe <132959114+Fialuxe@users.noreply.github.com>
2024-12-09 23:19:53 +0900

    [Update]ゲームクリアの状態を追加

--- State.java
+++ State.java
@@ -7,10 +7,12 @@ enum Event{
     STATE_3F,
     STATE_4F,
     STATE_GAMEOVER,
+    STATE_GAMECLEAR,
     STATE_1F_ROOM,
     STATE_2F_ROOM,
     STATE_3F_ROOM,
-    STATE_4F_ROOM
+    STATE_4F_ROOM,
+    STATE_HIDDEN_ROOM
 }
 /*
  * @brief
