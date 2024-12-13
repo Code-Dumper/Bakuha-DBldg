770878aa5fc31fb80e88757dd51dfb89f3f7c33a
Fialuxe <132959114+Fialuxe@users.noreply.github.com>
2024-12-13 16:32:52 +0900

    Panelの実装

---
+++ io/github/codedumper/view/FloorOnePanel.java
@@ -0,0 +1,5 @@
+package io.github.codedumper.view;
+
+public class FloorOnePanel extends JPanel{
+    
+}
---
+++ io/github/codedumper/view/FloorTwoPanel.java
@@ -0,0 +1,5 @@
+package io.github.codedumper.view;
+
+public class FloorTwoPanel extends JPanel{
+    
+}
