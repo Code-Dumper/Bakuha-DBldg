f91e9d07d1db0bc7a515a13b715f14ec6f7f8220
Fialuxe <132959114+Fialuxe@users.noreply.github.com>
2024-12-13 23:27:08 +0900

    [Update]色々おかしかった部分の修正

--- io/github/codedumper/view/GameViewPanel.java
+++ io/github/codedumper/view/GameViewPanel.java
@@ -5,6 +5,7 @@ import io.github.codedumper.controller.*;
 import javax.swing.*;
 import java.util.Observer;
 import java.util.Observable;
+import java.awt.*;
 
 @SuppressWarnings("deprecation")
 public class GameViewPanel extends JPanel implements Observer {
@@ -18,30 +19,33 @@ public class GameViewPanel extends JPanel implements Observer {
         this.controller = controller;
         model.addObserver(this); // Observerに登録
 
+        // タイマーのラベルを設定
         timerLabel = new JLabel("残り時間: " + model.getTimeRemaining() + "秒");
-        add(timerLabel, "North"); // ラベルを北側に配置
 
+        // メインパネルのレイアウト設定
+        setLayout(new BorderLayout());
+
+        // タイマーを北側に配置
+        add(timerLabel, BorderLayout.NORTH);
+
+        // 現在のパネルを中央に配置
         currentPanel = PanelFactory.createPanel(model.getCurrentState());
-        System.out.println("" + model.getCurrentState());
-        add(currentPanel, "Center");
+        add(currentPanel, BorderLayout.CENTER);
     }
 
     @Override
     public void update(Observable o, Object arg) {
-        //modelが時間変化、状態変化で送るメッセージ区別しているのでそこを区別して書く
         if (o instanceof GameModel) {
-            // 残り時間を更新
-            if("TIME_CHANGE".equals(arg)){
+            if ("TIME_CHANGE".equals(arg)) {
                 timerLabel.setText("残り時間: " + model.getTimeRemaining() + "秒");
-            }else if("STATE_CHANGE".equals(arg)){
-                // 状態に応じてパネルを切り替え
+            } else if ("STATE_CHANGE".equals(arg)) {
+                // 状態に応じてパネルを切り替える
                 remove(currentPanel);
                 currentPanel = PanelFactory.createPanel(model.getCurrentState());
-                add(currentPanel, "Center");
+                add(currentPanel, BorderLayout.CENTER);
                 revalidate();
                 repaint();
             }
-            
         }
     }
 }
