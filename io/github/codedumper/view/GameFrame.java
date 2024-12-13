08a10efe59d969f30dbf4db458fb1e4df6dd3b13
Fialuxe <132959114+Fialuxe@users.noreply.github.com>
2024-12-13 15:30:47 +0900

    [Why?]

---
+++ io/github/codedumper/view/GameFrame.java
@@ -0,0 +1,24 @@
+package io.github.codedumper.view;
+
+import javax.swing.JFrame;
+
+import io.github.codedumper.controller.*;
+import io.github.codedumper.model.*;
+
+class GameFrame extends JFrame {
+    GameModel model;
+    GameController controller;
+    GameView view;
+    public GameFrame() {
+        model = new GameModel();
+        controller = new GameController(model);
+        view = new GameView(model, controller);
+        this.setSize(800, 600);
+        this.add(view);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setVisible(true);
+    }
+    public static void main(String argv[]) {
+        new GameFrame();
+    }
+}
