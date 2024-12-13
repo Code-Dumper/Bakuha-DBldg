bb1c2e8bcc8cdf4f1df2709b5acf7dc78fb624f7
Fialuxe <132959114+Fialuxe@users.noreply.github.com>
2024-12-13 18:07:02 +0900

    [Why?]ペイントコンポーネントの削除とパネルの...?

--- io/github/codedumper/view/TitlePanel.java
+++ io/github/codedumper/view/TitlePanel.java
@@ -1,30 +1,23 @@
 package io.github.codedumper.view;
+
 import javax.swing.*;
 import java.awt.*;
-import java.awt.image.BufferedImage;
 
 public class TitlePanel extends JPanel {
-    private BufferedImage title;
     JPanel p1 = new JPanel(null);
 
     public TitlePanel() {
         JLabel b1 = new JLabel();
         try {
-            ImageIcon icon1 = new ImageIcon("title1.jpg");
+            ImageIcon icon1 = new ImageIcon(getClass().getResource("/io/github/codedumper/view/title.jpg"));
             b1.setIcon(icon1);
             b1.setPreferredSize(new Dimension(600, 200));
         } catch (Exception e) {
             e.printStackTrace();
+            System.out.println("画像の貼り付けに失敗しました");
         }
         b1.setBounds(0, 0, 600, 200);
-        p1.add(b1);
-    }
-
-    protected void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        if (title != null) {
-            g.drawImage(title, 0, 0, getWidth(), getHeight(), this);
-        }
+        this.add(b1);
     }
 
     public JPanel getTitlePanel() {
