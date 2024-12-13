1957ff1835667afc4a9555ab61bd9aa61c2bea50
Fialuxe <132959114+Fialuxe@users.noreply.github.com>
2024-12-13 16:25:06 +0900

    [Update]GameViewのクラス名変更による適切な処理の変更

--- io/github/codedumper/view/GameFrame.java
+++ io/github/codedumper/view/GameFrame.java
@@ -8,11 +8,11 @@ import io.github.codedumper.model.*;
 class GameFrame extends JFrame {
     GameModel model;
     GameController controller;
-    GameView view;
+    GameViewPanel view;
     public GameFrame() {
         model = new GameModel();
         controller = new GameController(model);
-        view = new GameView(model, controller);
+        view = new GameViewPanel(model, controller);
         this.setSize(800, 600);
         this.add(view);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
