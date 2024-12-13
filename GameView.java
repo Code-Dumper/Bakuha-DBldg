4b5c0f39de4b3a4e68e6db3375208632cacc3fb4
PON-de-quejo <t2311149@gl.cc.uec.ac.jp>
2024-12-13 15:32:38 +0900

    GameViewのコード

---
+++ GameView.java
@@ -0,0 +1,43 @@
+package io.github.codedumper.view;
+
+import javax.swing.*;
+import java.util.Observer;
+import java.util.Observable;
+
+public class GameView extends JFrame implements Observer {
+    private JLabel timerLabel;
+    private JPanel currentPanel;
+    private GameModel model;
+    private GameController controller;
+
+    public GameView(GameModel model, Gamecontroller controller) {
+        this.model = model;
+        this.controller = controller;
+        model.addObserver(this); // Observerに登録
+
+        timerLabel = new JLabel("残り時間: " + model.getTimeRemaining() + "秒");
+        add(timerLabel, "North"); // ラベルを北側に配置
+
+        currentPanel = PanelFactory.createPanel(model.getCurrentState());
+        add(currentPanel, "Center");
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(800, 600);
+        setVisible(true);
+    }
+
+    @Override
+    public void update(Observable o, Object arg) {
+        if (o instanceof GameModel) {
+            // 残り時間を更新
+            timerLabel.setText("残り時間: " + model.getTimeRemaining() + "秒");
+
+            // 状態に応じてパネルを切り替え
+            remove(currentPanel);
+            currentPanel = PanelFactory.createPanel(model.getCurrentState());
+            add(currentPanel, "Center");
+            revalidate();
+            repaint();
+        }
+    }
+}
