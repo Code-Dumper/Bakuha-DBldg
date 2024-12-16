40b5df920924970c771320da6ce5758b9367ab6b
PON-de-quejo <t2311149@gl.cc.uec.ac.jp>
2024-12-16 16:42:03 +0900

    [update]BasePanelクラスの修正

--- BasePanel.java
+++ BasePanel.java
@@ -1,21 +1,21 @@
+package io.github.codedumper.view;
+
+import io.github.codedumper.model.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import javax.swing.*;
+
 /*それぞれのPanelの基本的な機能持った親クラス
 このクラスを継承してそれぞれのPanelを作成する
 背景のセット、buttonの作成&貼り付け、状態遷移*/
 
-package io.github.codedumper.view;
-
-import io.githu.codedumper.model.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-
-import javax.swing.*;
-
 public class BasePanel extends JPanel implements ActionListener{
-    //コンポーネントを階層的に配置するpanel
-    //ボタンの上に画像を配置してボタンを見えなくする
+    /*コンポーネントを階層的に配置するpanel
+    ボタンの上に画像を配置してボタンを見えなくする*/
     private JLayeredPane panel;
 
     public BasePanel() {
+        setLayout(null); //親パネルのレイアウト無効化
         panel = new JLayeredPane();
         panel.setLayout(null);
         panel.setSize(600, 800);
@@ -24,23 +24,28 @@ public class BasePanel extends JPanel implements ActionListener{
     }
 
     //背景画像のset
-    public void setBackground(String path/*画像のpath*/) {
-        JLabel background = new JLabel(new ImageIcon(path));
+    public void setBackground(String path/*画像のclasspath*/) {
+        //画像のクラスパスを取得
+        ImageIcon imageIcon = new ImageIcon(getClass().getClassLoader().getResource(path));
+        JLabel background = new JLabel(imageIcon);
+
         background.setBounds(0, 0, 600, 800);
-        //背景をマウスイベント対象外にする
-        //これがないとクリックが背景画像に対して反応してしまいボタンが動作しない
+
+        /*背景をマウスイベント対象外にする
+        これがないとクリックが背景画像に対して反応してしまいボタンが動作しない*/
         background.addMouseListener(new java.awt.event.MouseAdapter() {});
+
         //背景をpanelの最上層に配置
         panel.add(background, Integer.valueOf(1));
     }
 
     
     //ボタンの作成、配置、貼り付け
-    public void createButton(String state, int x, int y, int width, int height) {
-            JButton button = new JButton(state);
+    public void createButton(Event event, ButtonLayout layout) {
+            JButton button = new JButton();
             //ボタンの位置指定
-            button.setBounds(x, y, width, height);
-            button.setActionCommand("state");
+            button.setBounds(layout.getX(), layout.getY(), layout.getWidth(), layout.getHeight());
+            button.setActionCommand(event);
             //ボタンをpanelの最下層に配置
             panel.add(button, Integer.valueOf(0));
             button.addActionListener(this);
@@ -48,22 +53,10 @@ public class BasePanel extends JPanel implements ActionListener{
 
     //遷移先をcontrollerに渡す
     public void actionPerformed(ActionEvent e) {
-        String es = e.getActionCommand();
-        switch(es) {
-            case "START":
-                GameController.transition(STATE_LOBBY);
-            case "END":
-                GameController.transition(STATE_GAMEOVER);
-            case "1F":
-                GameController.transition(STATE_FloorOnePanel);
-            case "2F":
-                GameController.transition(STATE_FloorTwoPanel);
-            case "3F":
-                GameController.transition(STATE_FloorThreePanel);
-            case "4F":
-                GameController.transition(STATE_FloorFourPanel);
-            default:
-                GameController.transition(STATE_TITLE);
-        }
+        //getActionCommand()はStringを返す
+        String state = e.getActionCommand();
+        //StringをEventに変換
+        Event destination = Event.valueOf(command);
+        GameController.transition(destination);
     }
 }
