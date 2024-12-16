1d474e916fdc047e109c1f030391d86bd9308012
Fialuxe <132959114+Fialuxe@users.noreply.github.com>
2024-12-16 19:12:51 +0900

    [Update]TitlePanelと同じようにインターフェース継承やコンストラクタを修正

--- io/github/codedumper/view/LobbyPanel.java
+++ io/github/codedumper/view/LobbyPanel.java
@@ -1,23 +1,24 @@
 package io.github.codedumper.view;
 
-import io.github.codedumper.model.*;
-import java.awt.*;
-import javax.swing.*;
+import io.github.codedumper.controller.*;
+import io.github.codedumper.model.Event;
+import java.awt.event.*;
 
 /* 背景画像の指定、ボタンの名前とサイズと位置指定*/
 
-public class LobbyPanel extends BasePanel implements ButtonLayout, ActionListener {
+public class LobbyPanel extends BasePanel{
 
-    public LobbyPanel() {
-    
+    public LobbyPanel(GameController controller) {
+        super(controller);
         //背景画像をset
         setBackground(getClass().getClassLoader().getResource("画像のクラスパス").getPath());
 
         //ボタンをset
-        createButton(STATE_1F, new ButtonLayout(0, 0, 100, 100));
-        createButton(STATE_2F, new ButtonLayout(0, 0, 100, 100));
-        createButton(STATE_3F, new ButtonLayout(0, 0, 100, 100));
-        createButton(STATE_4F, new ButtonLayout(0, 0, 100, 100));
+        //TODO この実装だと同じ位置にボタンを追加している。
+        createButton(Event.STATE_1F, new ButtonProperties(0, 0, 100, 100));
+        createButton(Event.STATE_2F, new ButtonProperties(0, 0, 100, 100));
+        createButton(Event.STATE_3F, new ButtonProperties(0, 0, 100, 100));
+        createButton(Event.STATE_4F, new ButtonProperties(0, 0, 100, 100));
         
     }
 
