fa915ecac4ea41808ccf81fd154ebb1fad89a9f1
Fialuxe <132959114+Fialuxe@users.noreply.github.com>
2024-12-24 22:16:25 +0900

    [Update]マウス操作用にControllerがGameViewPanelを受け取るように一部改変

--- io/github/codedumper/controller/GameController.java
+++ io/github/codedumper/controller/GameController.java
@@ -1,12 +1,23 @@
 package io.github.codedumper.controller;
 
+import java.awt.event.MouseAdapter;
+
 import io.github.codedumper.model.*;
-public class GameController {
+import io.github.codedumper.view.GameViewPanel;
+public class GameController extends MouseAdapter{
+
     protected GameModel model;
-    public GameController(GameModel model) {
+    private GameViewPanel view;
+    public GameController(GameModel model, GameViewPanel view) {
         this.model = model;
+        this.view = view;
+        view.addMouseListener(this);
+        view.addMouseMotionListener(this);
     }
+
     public void transition(Event event) {
         model.setCurrentState(event);
     }
+
+    
 }
