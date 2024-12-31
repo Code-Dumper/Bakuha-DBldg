9ca8e4cc850f8bbca963947027374afade1defe5
Fialuxe <132959114+Fialuxe@users.noreply.github.com>
2024-12-31 15:57:30 +0900

    [Update]爆弾解除の戦略を一度まっさらにする

--- io/github/codedumper/model/bomb/PuzzleDisarmsStrategy.java
+++ io/github/codedumper/model/bomb/PuzzleDisarmsStrategy.java
@@ -3,11 +3,10 @@ package io.github.codedumper.model.bomb;
 import io.github.codedumper.model.GameModel;
 
 //パズルを解除の鍵とした爆弾解除戦略クラス
-public class PuzzleDisarmsStrategy implements DisarmStrategy{
-
-    private final GameModel model;
-    public PuzzleDisarmsStrategy(GameModel model){
-        this.model = model;
+public class PuzzleDisarmsStrategy extends BaseDisarmStrategy{
+    
+    public PuzzleDisarmsStrategy(GameModel model, Boolean key){
+        super(model, key);
     }
 
     public boolean canDisarm(Bomb bomb, Object input){
