a0d087ed7333df08f90a510231233f8936947bcc
Fialuxe <132959114+Fialuxe@users.noreply.github.com>
2024-12-31 15:56:46 +0900

    [Update]DisarmStrategyの仕様を少し決める

---
+++ io/github/codedumper/model/bomb/BaseDisarmStrategy.java
@@ -0,0 +1,18 @@
+package io.github.codedumper.model.bomb;
+
+import io.github.codedumper.model.GameModel;
+
+//爆弾解除の方法が持っておくべき基本の性質を定義したクラス。
+
+public class BaseDisarmStrategy{
+    protected GameModel model;
+    protected Object key;
+    
+    public BaseDisarmStrategy(GameModel model, Object key){
+        this.model = model;
+        this.key = key;
+    }
+    public boolean canDisarm(Bomb bomb, Object input){
+        return false;
+    };
+}
