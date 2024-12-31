963ce9611e006697b86219a1339b2171a7bfdbe2
Fialuxe <132959114+Fialuxe@users.noreply.github.com>
2024-12-31 19:04:40 +0900

    [Update] STATE

--- io/github/codedumper/model/StateMachine.java
+++ io/github/codedumper/model/StateMachine.java
@@ -27,7 +27,39 @@ public class StateMachine {
                                         Event.STATE_3F, 
                                         Event.STATE_4F, 
                                         Event.STATE_GAMEOVER));
-
+        // 状態「STATE_1F」の遷移リスト
+        transitions.put(Event.STATE_1F,
+                        Arrays.asList(  Event.STATE_LOBBY,
+                                        Event.STATE_1F_ROOM,
+                                        Event.STATE_1F_BOMB));
+        // 状態「STATE_2F」の遷移リスト
+        transitions.put(Event.STATE_2F,
+                        Arrays.asList(  Event.STATE_LOBBY,
+                                        Event.STATE_2F_ROOM,
+                                        Event.STATE_2F_BOMB));
+        // 状態「STATE_3F」の遷移リスト
+        transitions.put(Event.STATE_3F,
+                        Arrays.asList(  Event.STATE_LOBBY,
+                                        Event.STATE_3F_ROOM,
+                                        Event.STATE_3F_BOMB));
+        // 状態「STATE_4F」の遷移リスト
+        transitions.put(Event.STATE_4F,
+                        Arrays.asList(  Event.STATE_LOBBY,
+                                        Event.STATE_4F_ROOM,
+                                        Event.STATE_4F_BOMB));
+        // 状態「STATE_1F_BOMB」
+        transitions.put(Event.STATE_1F_BOMB,
+                        Arrays.asList(  Event.STATE_1F));
+        // 状態「STATE_2F_BOMB」
+        transitions.put(Event.STATE_2F_BOMB,
+                        Arrays.asList(  Event.STATE_2F));
+        // 状態「STATE_3F_BOMB」
+        transitions.put(Event.STATE_3F_BOMB,
+                        Arrays.asList(  Event.STATE_3F));
+        // 状態「STATE_4F_BOMB」
+        transitions.put(Event.STATE_2F_BOMB,
+                        Arrays.asList(  Event.STATE_4F));  
+                                     
         // 状態「STATE_GAMEOVER」の遷移リスト
         transitions.put(Event.STATE_GAMEOVER, 
                         Arrays.asList(  Event.STATE_TITLE, 
