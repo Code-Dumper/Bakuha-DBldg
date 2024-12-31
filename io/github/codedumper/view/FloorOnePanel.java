3bdebbab2bcb8eb0e74f9fa49078c4a05f44c3b2
Fialuxe <132959114+Fialuxe@users.noreply.github.com>
2024-12-31 19:23:19 +0900

    [Update]Panelの雛形を作成
    あとは画像ができればすぐに作成できるように。

--- io/github/codedumper/view/FloorOnePanel.java
+++ io/github/codedumper/view/FloorOnePanel.java
@@ -4,11 +4,11 @@ import java.awt.event.ActionEvent;
 import io.github.codedumper.controller.GameController;
 import io.github.codedumper.model.Event;
 
-public class FloorFourPanel extends BasePanel{
-    public FloorFourPanel(GameController controller){
+public class FloorOnePanel extends BasePanel{
+    public FloorOnePanel(GameController controller){
         super(controller);
-        this.createButton(Event.STATE_4F_BOMB, new ButtonProperties(100, 100, 100, 100));
-        this.createButton(Event.STATE_4F_ROOM, new ButtonProperties(200, 200, 100, 100));
+        this.createButton(Event.STATE_1F_BOMB, new ButtonProperties(100, 100, 100, 100));
+        this.createButton(Event.STATE_1F_ROOM, new ButtonProperties(200, 200, 100, 100));
     }
     @Override
     public void actionPerformed(ActionEvent e) {
