10f3686cb627546cb9099a5a4dee2d0ca0418003
Fialuxe <132959114+Fialuxe@users.noreply.github.com>
2024-12-31 23:54:26 +0900

    [Update]後々番号を変えやすいように定数クラスを作成

---
+++ io/github/codedumper/model/Constant.java
@@ -0,0 +1,8 @@
+package io.github.codedumper.model;
+
+public class Constant {
+    public static int oneFloorBombCode = 1234;
+    public static int twoFloorBombCode = 3456;
+    public static int threeFloorBombCode = 5678;
+    public static int fourFloorBombCode = 7890;
+}
