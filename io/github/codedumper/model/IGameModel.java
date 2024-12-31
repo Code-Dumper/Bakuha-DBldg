ea7d942daeac9cae2ade5c39aa32a2b3cbe3dee3
Fialuxe <132959114+Fialuxe@users.noreply.github.com>
2025-01-01 01:18:35 +0900

    [Update]GameModelのインターフェース実装

--- io/github/codedumper/model/IGameModel.java
+++ io/github/codedumper/model/IGameModel.java
@@ -7,10 +7,10 @@ import io.github.codedumper.model.planarity.Edge;
 import java.awt.Point;
 
 public interface IGameModel {
-    public int getTimeRemaining();
+    public double getTimeRemaining();
     public void setCurrentState(Event event);
     public Event getCurrentState();
-    public void onTimeChange(int newTime);
+    public void onTimeChange(double newTime);
     public void onTimeOut();
     public List<Point> getNodes();
     public void moveNode(int index, Point newPosition);
@@ -18,5 +18,5 @@ public interface IGameModel {
     public void recreatePuzzle();
     public List<Edge> getEdges();
     public List<Edge> getIntersectingEdges();
-    public void disarmBomb(Event e);
+    public boolean disarmBomb(Event e);
 }
