54bbbcca4d6ad80561a17026791d07e471d04eea
Fialuxe <132959114+Fialuxe@users.noreply.github.com>
2024-12-31 15:57:55 +0900

    [Update]BombManagerの管理する爆弾を削除する

--- io/github/codedumper/model/bomb/Bomb.java
+++ io/github/codedumper/model/bomb/Bomb.java
@@ -2,11 +2,13 @@ package io.github.codedumper.model.bomb;
 
 import io.github.codedumper.model.GameModel;
 
-public class Bomb {
-    private final DisarmStrategy disarmStrategy;
+
+
+public class Bomb{
+    private BaseDisarmStrategy disarmStrategy;
     private boolean disarmed;
 
-    public Bomb(GameModel model, DisarmStrategy disarmStrategy) {
+    public Bomb(GameModel model, BaseDisarmStrategy disarmStrategy) {
         this.disarmStrategy = disarmStrategy;
         this.disarmed = false;
     }
--- io/github/codedumper/model/bomb/BombManager.java
+++ io/github/codedumper/model/bomb/BombManager.java
@@ -6,20 +6,23 @@ import java.util.List;
 import io.github.codedumper.model.GameModel;
 
 /*
- * 爆弾解除と爆弾情報を担うマネージャクラス。
+ * 爆弾解除と爆弾情報を担うクラス。
  */
+
 public class BombManager {
+
     GameModel model;
     List<Bomb> bombs;
+
     public BombManager(GameModel model){
         this.model = model;
         bombs = new ArrayList<Bomb>();
-        addBomb(model, new CodeDisarmStrategy("1234"));
-        addBomb(model, new PuzzleDisarmsStrategy(model)); 
     }
-    public void addBomb(GameModel model, DisarmStrategy disarmStrategy){
+
+    public void addBomb(GameModel model, BaseDisarmStrategy disarmStrategy){
         bombs.add(new Bomb(model, disarmStrategy));
     }
+    
     public void disarmBomb(int index, Object input){
         if(index < 0 || bombs.size() <= index){
             throw new IllegalArgumentException();
