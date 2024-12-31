de179a40f541b0f51006ca1bb2b0a0e235ee6ca5
Fialuxe <132959114+Fialuxe@users.noreply.github.com>
2024-12-31 18:56:24 +0900

    [Update]ロビーの仮実装

--- io/github/codedumper/view/LobbyPanel.java
+++ io/github/codedumper/view/LobbyPanel.java
@@ -11,14 +11,14 @@ public class LobbyPanel extends BasePanel{
     public LobbyPanel(GameController controller) {
         super(controller);
         //背景画像をset
-        this.setBackground("io/github/codedumper/view/LobbyPicture.png", 1);
+        this.setBackground("io/github/codedumper/view/image-lobby.jpg", 1);
 
         //ボタンをset
         //TODO この実装だと同じ位置にボタンを追加している。
         createButton(Event.STATE_1F, new ButtonProperties(0, 0, 100, 100));
-        createButton(Event.STATE_2F, new ButtonProperties(0, 0, 100, 100));
-        createButton(Event.STATE_3F, new ButtonProperties(0, 0, 100, 100));
-        createButton(Event.STATE_4F, new ButtonProperties(0, 0, 100, 100));
+        createButton(Event.STATE_2F, new ButtonProperties(0, 0, 100, 200));
+        createButton(Event.STATE_3F, new ButtonProperties(0, 0, 100, 300));
+        createButton(Event.STATE_4F, new ButtonProperties(0, 0, 100, 400));
         
     }
 
