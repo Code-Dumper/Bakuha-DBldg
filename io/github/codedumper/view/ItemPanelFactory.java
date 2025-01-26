d51cceb496cb6d669851f83be9f636b4f44a9e1e
Fialuxe <132959114+Fialuxe@users.noreply.github.com>
2025-01-27 01:20:58 +0900

    Update ItemPanelFactory.java

--- io/github/codedumper/view/ItemPanelFactory.java
+++ io/github/codedumper/view/ItemPanelFactory.java
@@ -2,15 +2,17 @@ package io.github.codedumper.view;
 
 import javax.swing.*;
 
+import io.github.codedumper.controller.GameController;
+
 class ItemPanelFactory {
-    public static JPanel createItemPanel(String item) {
+    public static JPanel createItemPanel(String item, GameController controller) {
         JPanel currentItemPanel;
         switch(item) {
             case "2F_ROOM":
-                currentItemPanel = new RoomTwoPanel();
+                currentItemPanel = new RoomTwoPanel(controller);
                 break;
             case "eatora":
-                currentItemPanel = new eatoraPanel();
+                currentItemPanel = new eatoraPanel(controller);
                 break;
             default:
                 currentItemPanel = null;
