5f886346a6d8f3052ec5dde143f9d6d822462475
PON-de-quejo <t2311149@gl.cc.uec.ac.jp>
2025-01-26 12:49:51 +0900

    [update]遷移先のパネルが階層構造をもつようにcreatePanelメソッドで生成されるパネルをFandamentalPanel型に変更

--- io/github/codedumper/view/PanelFactory.java
+++ io/github/codedumper/view/PanelFactory.java
@@ -14,8 +14,8 @@ import io.github.codedumper.model.GameModel;
  * ...
  */
 class PanelFactory{
-    public static JPanel createPanel(Event event, GameModel model, GameController controller){
-        JPanel productPanel;
+    public static FandamentalPanel createPanel(Event event, GameModel model, GameController controller){
+        FandamentalPanel productPanel;
         switch(event){
             case STATE_TITLE:
                 productPanel = new TitlePanel(controller);
