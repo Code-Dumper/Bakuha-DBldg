b6bb2bb22027141dd863fbed08a2452ab5c8fbf9
Fialuxe <132959114+Fialuxe@users.noreply.github.com>
2025-01-30 17:50:08 +0900

    [update]LobbyPanelのボタンの位置を変更

--- io/github/codedumper/view/LobbyPanel.java
+++ io/github/codedumper/view/LobbyPanel.java
@@ -11,14 +11,14 @@ public class LobbyPanel extends BasePanel{
     public LobbyPanel(GameController controller) {
         super(controller);
         //背景画像をset
-        this.setBackground("io/github/codedumper/view/image-lobby.jpg", 1);
+        this.setBackground("io/github/codedumper/view/image-Information.jpg", 1);
 
         //ボタンをset
         //TODO この実装だと同じ位置にボタンを追加している。
-        createButton(State.STATE_1F, new ButtonProperties(0, 0, 100, 100));
-        createButton(State.STATE_2F, new ButtonProperties(0, 0, 100, 200));
-        createButton(State.STATE_3F, new ButtonProperties(0, 0, 100, 300));
-        createButton(State.STATE_4F, new ButtonProperties(0, 0, 100, 400));
+        createButton(State.STATE_4F, new ButtonProperties(200, 370, 200, 30));
+        createButton(State.STATE_3F, new ButtonProperties(200, 420, 200, 30));
+        createButton(State.STATE_2F, new ButtonProperties(200, 490, 200, 30));
+        createButton(State.STATE_1F, new ButtonProperties(200, 550, 200, 30));
         
     }
 
