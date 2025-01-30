39b7dd6de6d50b7165e3b0b34c9f3cc7c04c8773
Fialuxe <132959114+Fialuxe@users.noreply.github.com>
2025-01-30 19:19:19 +0900

    [add]デジタルフォント

--- io/github/codedumper/model/Constant.java
+++ io/github/codedumper/model/Constant.java
@@ -1,5 +1,10 @@
 package io.github.codedumper.model;
 
+import java.awt.Font;
+import java.awt.FontFormatException;
+import java.io.InputStream;
+import java.io.IOException;
+
 public class Constant {
     public static int oneFloorBombCode = 1234;
     public static int twoFloorBombCode = 3456;
