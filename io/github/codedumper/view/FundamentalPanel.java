8e8889ddcb3e415e7f86f7e9f1e5ea0b5bc5eafe
PON-de-quejo <t2311149@gl.cc.uec.ac.jp>
2025-02-04 09:27:03 +0900

    [update]Font元の形に戻した

--- io/github/codedumper/view/FundamentalPanel.java
+++ io/github/codedumper/view/FundamentalPanel.java
@@ -329,26 +329,10 @@ public abstract class FundamentalPanel extends JPanel implements ActionListener{
         inputNumbers = new StringBuilder();
         displayLabel = new JLabel("", SwingConstants.CENTER);
         displayLabel.setBounds(33, 75, 532, 122);
-        displayLabel.setFont(createFont("DS-DIGI.TTF", 90));
+        displayLabel.setFont(new Font("Arial", Font.BOLD, 90));
         displayLabel.setBorder(BorderFactory.createLineBorder(Color.black));
         layeredPane.add(displayLabel, LAYER_UTIL_SECOND);
         return displayLabel;
-    }
-
-    public Font createFont(String filename, float size){
-        Font font = null;
-        InputStream is = null;
-        try{
-            is = getClass().getResourceAsStream("DS-DIGI.TTF");
-            font = Font.createFont(Font.TRUETYPE_FONT,is);
-            font = font.deriveFont(size);
-            is.close();
-        }catch(IOException e){
-            e.printStackTrace();
-        }catch(FontFormatException e){
-            e.printStackTrace();
-        }
-        return font;
     }
 
     //数字入力用のボタンを配置するパネル
