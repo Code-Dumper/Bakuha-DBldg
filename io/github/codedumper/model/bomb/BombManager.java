134cf79c14049bcea0fc1fb0380e97df9d6d66b7
Fialuxe <132959114+Fialuxe@users.noreply.github.com>
2025-02-04 15:21:35 +0900

    [update][update] GameModelの役割分担パート3 爆弾管理をBombManagerに完全に委任する

--- io/github/codedumper/model/bomb/BombManager.java
+++ io/github/codedumper/model/bomb/BombManager.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import io.github.codedumper.model.GameModel;
+import io.github.codedumper.model.State;
 
 /*
  * 爆弾解除と爆弾情報を担うクラス。
@@ -33,6 +34,69 @@ public class BombManager {
         }
         Bomb bombToDisarm = bombs.get(index);
         return bombToDisarm.disarm(input);
+    }
+
+    public int getBombCodeOf(State currentState){
+        int keyIndex;
+        switch(currentState){
+            case STATE_1F:  keyIndex = 1;   break;
+            case STATE_2F:  keyIndex = 2;   break;
+            case STATE_3F:  keyIndex = 3;   break;
+            case STATE_4F:  keyIndex = 4;   break;
+            default:        keyIndex = 0;
+        }
+        return bombs.get(keyIndex).getCode();
+    }
+
+    public void inputCodeTo(State currentState, int value){
+        int keyIndex;
+        switch(currentState){
+            case STATE_1F:  keyIndex = 1;   break;
+            case STATE_2F:  keyIndex = 2;   break;
+            case STATE_3F:  keyIndex = 3;   break;
+            case STATE_4F:  keyIndex = 4;   break;
+            default:        keyIndex = 0;
+        }
+        //何らかの要因で予定していた状態以外から入力が入ったら破棄する
+        if(keyIndex == 0) return;
+        bombs.get(keyIndex).inputCode(value);
+    }
+
+    public boolean disarmBomb(State currentState){
+        int keyIndex;
+        switch(currentState){
+            case STATE_1F: keyIndex = 1; break;
+            case STATE_2F: keyIndex = 2; break;
+            case STATE_3F: keyIndex = 3; break;
+            case STATE_4F: keyIndex = 4; break;
+            default:       keyIndex = 0; break;
+        }
+        if(keyIndex == 0){return false;}
+        return bombs.get(keyIndex).disarm(getBombCodeOf(currentState));
+    }
+
+    public boolean isDisarmedBombOf(State currentState){
+        int keyIndex;
+        switch(currentState){
+            case STATE_1F: keyIndex = 1; break;
+            case STATE_2F: keyIndex = 2; break;
+            case STATE_3F: keyIndex = 3; break;
+            case STATE_4F: keyIndex = 4; break;
+            default:       keyIndex = 0; break;
+        }
+        return bombs.get(keyIndex).isDisarmed();
+    }
+
+    public void resetCode(State currentState){
+        int keyIndex;
+        switch(currentState){
+            case STATE_1F: keyIndex = 1; break;
+            case STATE_2F: keyIndex = 2; break;
+            case STATE_3F: keyIndex = 3; break;
+            case STATE_4F: keyIndex = 4; break;
+            default:       keyIndex = 0; break;
+        }
+        bombs.get(keyIndex).resetCode();
     }
 
     public boolean areAllBombsDisarmed(){
