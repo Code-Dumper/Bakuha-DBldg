49a985b56cb9b58b32d0ba944acf632be9b8effb
Fialuxe <132959114+Fialuxe@users.noreply.github.com>
2025-02-04 15:20:37 +0900

    [update] GameModelの役割分担パート1

--- io/github/codedumper/model/GameModel.java
+++ io/github/codedumper/model/GameModel.java
@@ -18,47 +18,41 @@ public class GameModel extends Observable implements IGameModel{
     
     private       State currentState; //現在のゲーム状態
     private final GameTimer timer; //ゲームで共通のタイマー
-    private       double remainTime; //残り時間
     private final BombManager bombManager; //爆弾管理
     private final StateMachine stateMachine; //状態管理
     private final GraphManager graphManager; //ミニゲームのグラフ管理
     public  final DataManager dataManager;
-    private final int key[]; //ユーザの入力した爆弾解除のコード
 
     //残り時間をINITIAL_TIME(3600s)、初期状態をタイトル状態
     public GameModel(){
         currentState = State.STATE_TITLE;
-        remainTime = INITIAL_TIME;
         timer = new GameTimer(INITIAL_TIME, this, this);
         dataManager = new DataManager(60);
         bombManager = new BombManager(this);
         stateMachine = new StateMachine();
         graphManager = new GraphManager(this);
-        key = new int[5];
         timer.start();
     }
 
     //残り時間の通知
     public final synchronized double getTimeRemaining(){
-        return remainTime;
+        return timer.getRemainingTime();
     }
     //時間変化時の更新処理
     @Override
     public final void onTimeChange(double newTime){
-        remainTime = newTime;
-        notifyTimeChange();
+        this.notifyTimeChange();
     }
     //時間切れの時の処理
     @Override
     public final void onTimeOut(){
-        setCurrentState(State.STATE_GAMEOVER);
+        this.setCurrentState(State.STATE_GAMEOVER);
     }
 
     //状態遷移
     public final synchronized void setCurrentState(State event){
-        if(event == State.STATE_END){
-            System.exit(0);
-        }
+        //ゲームを終了する
+        if(event == State.STATE_END){ System.exit(0);}
         currentState = stateMachine.getNextState(currentState, event);
         notifyStateChange();
     }
@@ -93,69 +87,20 @@ public class GameModel extends Observable implements IGameModel{
     }
     //爆弾のコード入力用の処理
     public final void inputCode(int input){
-        int keyIndex;
-        switch(currentState){
-            case STATE_1F:
-                keyIndex = 1;
-                break;
-            case STATE_2F:
-                keyIndex = 2;
-                break;
-            case STATE_3F:
-                keyIndex = 3;
-                break;
-            case STATE_4F:
-                keyIndex = 4;
-                break;
-            default:
-                keyIndex = 0;
-        }
-        //4桁以上入力されていたら以降の入力は破棄する
-        if(key[keyIndex] >= 9000) return;
-        //何らかの要因で予定していた状態以外から入力が入ったら破棄する
-        if(keyIndex == 0) return;
-        key[keyIndex] = key[keyIndex] * 10 + input;
-        
+        bombManager.inputCodeTo(currentState, input);
     }
 
     public final void resetCode(){
-        int keyIndex;
-        switch(currentState){
-            case STATE_1F:  keyIndex = 1;   break;
-            case STATE_2F:  keyIndex = 2;   break;
-            case STATE_3F:  keyIndex = 3;   break;
-            case STATE_4F:  keyIndex = 4;   break;
-            default:        keyIndex = 0;
-        }
-        key[keyIndex] = 0;
+        bombManager.resetCode(currentState);
     }
 
     public final int getCurrentCode(){
-        int keyIndex;
-        switch(currentState){
-            case STATE_1F:  keyIndex = 1;   break;
-            case STATE_2F:  keyIndex = 2;   break;
-            case STATE_3F:  keyIndex = 3;   break;
-            case STATE_4F:  keyIndex = 4;   break;
-            default:        keyIndex = 0;
-        }
-        return key[keyIndex];
+        return this.bombManager.getBombCodeOf(currentState);
     }
 
     //eに合わせて爆弾を取得し、その解除を試みる
     public final boolean disarmBomb(){
-        switch(currentState){
-            case STATE_1F: 
-                return bombManager.disarmBomb(1, key[1]);
-            case STATE_2F: 
-                return bombManager.disarmBomb(2, key[2]);
-            case STATE_3F:
-                return bombManager.disarmBomb(3,key[3]);
-            case STATE_4F:
-                return bombManager.disarmBomb(4,key[4]);
-            default:
-                return false;
-            }
+        return this.bombManager.disarmBomb(currentState);
     }
 
     public final boolean areAllBombsDisarmed(){
